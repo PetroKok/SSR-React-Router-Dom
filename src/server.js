0d29f2f035429d4618ee68872951cb81cc0b138e
Petro <kokpetro15@gmail.com>
2018-02-23 14:31:36 +0200

    add css bootstrap4 to the server file

--- src/server.js
+++ src/server.js
@@ -39,6 +39,7 @@ function render(html, loadableState) {
         <head>
             <meta charset="UTF-8">
             <title>Get real playlists to share with Spotify</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         </head>
         <body>
            
