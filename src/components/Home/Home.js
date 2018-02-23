bf36517de3b06b8299d77884bc5347ccaf4f4247
Petro <kokpetro15@gmail.com>
2018-02-23 15:02:57 +0200

    added photo of cars

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,9 +4,12 @@ class Home extends Component
 {
     render()
     {
-        this.props.data(1);
         return(
-            <div>This is the class Home</div>
+            <div>
+                    <img className="img-fluid" src="http://citydrivered.com/wp-content/uploads/2015/04/slide-2-1366x500.jpg" />
+                    <img className="img-fluid" src="https://www.superauto.com.au/wp-content/uploads/2017/10/RED-JAG-3-1366x500.jpg" />
+                    <img className="img-fluid" src="http://hddesktopwallpapers.in/wp-content/uploads/2015/07/ferrari-458-italia-yellow-hd-1366x500.jpg" />
+            </div>
         );
     }
 }
